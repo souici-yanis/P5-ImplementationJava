cc586feb627531a2e021186f7c937aee6089129d
souici-yanis <souici.yanis@gmail.com>
2020-04-07 23:58:12 +0200

    fix float calcul

--- src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
+++ src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
@@ -9,27 +9,28 @@ public class FareCalculatorService {
         if ((ticket.getOutTime() == null) || (ticket.getOutTime().before(ticket.getInTime()))) {
             throw new IllegalArgumentException("Out time provided is incorrect:" + ticket.getOutTime().toString());
         }
-
+        
         Long inHour = ticket.getInTime().getTime();
         System.out.println("inHour " + inHour);
         Long outHour = ticket.getOutTime().getTime();
         System.out.println("outHour " + outHour);
 
         //TODO: Some tests are failing here. Need to check if this logic is correct
+        long duration = (outHour - inHour);
         int timeStampConverted = 3600000; // (1000 * 60 * 60);
-        long duration = (outHour - inHour) / timeStampConverted; // Convert milisecond to Hour
-        System.out.println("duration " + duration);
+        float durationConverted = (float) duration / timeStampConverted; // Convert milisecond to Hour
 
 
-        if (duration <= 0.5) {
+
+        if (durationConverted <= 0.5) {
             ticket.setPrice(0);
         } else {
             switch (ticket.getParkingSpot().getParkingType()){
                 case CAR: 
-                    ticket.setPrice(duration * Fare.CAR_RATE_PER_HOUR);
+                    ticket.setPrice(durationConverted * Fare.CAR_RATE_PER_HOUR);
                     break;
                 case BIKE: 
-                    ticket.setPrice(duration * Fare.BIKE_RATE_PER_HOUR);
+                    ticket.setPrice(durationConverted * Fare.BIKE_RATE_PER_HOUR);
                     break;
             }
         }
